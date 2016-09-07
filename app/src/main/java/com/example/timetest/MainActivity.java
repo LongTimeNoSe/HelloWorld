50aa8b82ad12c4789adc04f8f4052d02ccbc0c2d
MissYouAllTheTme <842959627@qq.com>
2016-09-07 15:11:33 +0800

    MainActivity

--- app/src/main/java/com/example/timetest/MainActivity.java
+++ app/src/main/java/com/example/timetest/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.timetest;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.RelativeLayout;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RelativeLayout countDown;
+    private LinearLayout countDown;
     // 倒计时
     private TextView daysTv, hoursTv, minutesTv, secondsTv;
     private long mDay = 2;
@@ -50,17 +54,21 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+    private EditText name, paw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        countDown = (RelativeLayout) findViewById(R.id.countdown_layout);
+        countDown = (LinearLayout) findViewById(R.id.countdown_layout);
         daysTv = (TextView) findViewById(R.id.days_tv);
         hoursTv = (TextView) findViewById(R.id.hours_tv);
         minutesTv = (TextView) findViewById(R.id.minutes_tv);
         secondsTv = (TextView) findViewById(R.id.seconds_tv);
+
+        name = (EditText) findViewById(R.id.name);
+        paw = (EditText) findViewById(R.id.paw);
 
         startRun();
     }
@@ -106,5 +114,56 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    public void next(View view) {
+        Intent intent = new Intent(MainActivity.this, ResultIntentActivity.class);
+        startActivityForResult(intent, 1);
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Warning");
+        dialog.setMessage("You are forced to be offline , Please try to login again");
+        dialog.setCancelable(false);
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ActivityCollector.finishAllActivity();
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+        dialog.create().show();
+    }
+
+    public void login(View view) {
+
+        String userName = name.getText().toString();
+        String password = paw.getText().toString();
+        if (userName.equals("a") && password.equals("a")) {
+            startActivity(new Intent(MainActivity.this, ResultIntentActivity.class));
+        } else {
+            Toast.makeText(MainActivity.this, "用户名或密码错误", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void dataSave(View view) {
+        startActivity(new Intent(MainActivity.this, SaveDataActivity.class));
+    }
+
+    public void notification(View view) {
+        startActivity(new Intent(MainActivity.this, NotificationActivity.class));
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            String result = data.getStringExtra("backData");
+            Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
